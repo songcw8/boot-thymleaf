244cdb24849a781f0b64fc5e74b87771ff4f78bb
songcw8 <ben990158@gmail.com>
2025-04-09 18:11:06 +0900

    끝말잇기

--- src/main/java/org/example/bootthymleaf/controller/MainController.java
+++ src/main/java/org/example/bootthymleaf/controller/MainController.java
@@ -1,17 +1,21 @@
 package org.example.bootthymleaf.controller;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.apache.catalina.LifecycleState;
+import org.example.bootthymleaf.model.dto.UpdateWordForm;
 import org.example.bootthymleaf.model.dto.WordForm;
 import org.example.bootthymleaf.model.entity.Word;
 import org.example.bootthymleaf.model.repository.WordRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.text.Normalizer;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -58,6 +62,27 @@ public class MainController {
         Word word = new Word();
         word.setText(wordForm.getWord());
         wordRepository.save(word);
+        return "redirect:/";
+    }
+
+    @PostMapping("/update")
+    @Transactional // 최종해결
+    public String updateWord(@ModelAttribute UpdateWordForm form, RedirectAttributes redirectAttributes) {
+        // JPA는 업데이트용 메서드나 기능이 따로 없어요
+        // JPA는 수정용이 따로 없어요
+        // -> 교체 개념이에요 => put <-> patch : 멱등성 (TIL)
+        // JPA : Jakarta Persistence API
+        Word oldWord = wordRepository.findById(form.getUuid()).orElseThrow();
+        oldWord.setText(form.getNewWord());
+        wordRepository.save(oldWord);
+        redirectAttributes.addFlashAttribute("message", "%s: 단어가 정상적으로 교체되었습니다.".formatted(oldWord.getText()));
+        return "redirect:/";
+    }
+
+    @PostMapping("/delete")
+    public String deleteWord(@RequestParam("id") String uuid, @RequestParam("word") String text, RedirectAttributes redirectAttributes) {
+        wordRepository.deleteById(uuid);
+        redirectAttributes.addFlashAttribute("message", "%s: 단어가 정상적으로 삭제되었습니다.".formatted(text));
         return "redirect:/";
     }
 }
---
+++ src/main/java/org/example/bootthymleaf/model/dto/UpdateWordForm.java
@@ -0,0 +1,9 @@
+package org.example.bootthymleaf.model.dto;
+
+import lombok.Data;
+
+@Data
+public class UpdateWordForm {
+    private String newWord;
+    private String uuid;
+}
