c0f8edefe0b36074bfcc8b1fdb7744ebfd6c4ea4
songcw8 <ben990158@gmail.com>
2025-04-09 23:17:31 +0900

    fix: TimeZone seoul

--- src/main/java/org/example/bootthymleaf/BootThymleafApplication.java
+++ src/main/java/org/example/bootthymleaf/BootThymleafApplication.java
@@ -3,10 +3,13 @@ package org.example.bootthymleaf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BootThymleafApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul")); // 여기에 추가
         SpringApplication.run(BootThymleafApplication.class, args);
     }
 
